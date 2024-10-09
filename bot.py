1263359b1cb61d90dae83021d15cec9a62c8a035
yenngu <156570816+yenngu@users.noreply.github.com>
2024-10-09 18:45:13 -0400

    implemented functioanlity to have continuous conversations with bot

--- bot.py
+++ bot.py
@@ -4,17 +4,37 @@ import os
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-def call_assistant(user_message):
+stored_responses = []
+
+def call_assistant(user_message, stored_responses):
+    messages = [{"role": "system", "content": "You are a professional doctor that helps clients and provides detailed analysis"}]
+    for history in stored_responses:
+        messages.append({"role": "user", "content": history['user']})
+        messages.append({"role": "assistant", "content": history['assistant']})
+    
+    messages.append({"role": "user", "content": user_message})
+    
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role" :"system", "content": "You are a personal nurse, skilled in the medical field, give detailed responses like a professional. Only respond if it's medical related."},
-            {"role" :"user", "content": user_message}
-        ]   
+        messages=messages
     )
 
     response = completion.choices[0].message.content
     print(response)
+    stored_responses.append({"user": user_message, "assistant": response})
     return response
 
-call_assistant(input("What would you like to ask NurseBot?"))
+
+while True:
+    userInput = input("What would you like to ask the NurseBot? (Type 'exit' to exit)")
+    
+    if userInput.lower() == 'exit':
+        print("Thank you for using NurseBot")
+        break   
+    
+    call_assistant(userInput, stored_responses)
+    
+    
+    
+    
+    

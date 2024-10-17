927ce7ac32051c3871453c9413b35ddd4a56c66e
yenngu <156570816+yenngu@users.noreply.github.com>
2024-10-17 01:15:31 -0400

    designed frontend

--- nurseapp/src/App.js
+++ nurseapp/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="top-section">
+        <h1> Nurse Bot</h1>
+      </div>
+
+      <div class="result-container">
+        <textarea readOnly>
+
+        </textarea>
+      </div>
+      <div class="user-container">
+        <input></input>
+        <button className="submit-button"> Ask Bot... </button>
+      </div>
+    </>
   );
 }
 
